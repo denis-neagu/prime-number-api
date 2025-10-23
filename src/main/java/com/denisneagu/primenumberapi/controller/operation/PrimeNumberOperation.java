2f6b22fe7f77da078218a5c8611e6130ee0dc0a1
denis-neagu <denis.neagu@outlook.com>
2025-10-23 21:28:40 +0100

    add @GetMapping

--- src/main/java/com/denisneagu/primenumberapi/controller/operation/PrimeNumberOperation.java
+++ src/main/java/com/denisneagu/primenumberapi/controller/operation/PrimeNumberOperation.java
@@ -1,12 +1,15 @@
 package com.denisneagu.primenumberapi.controller.operation;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Validated
 @RequestMapping("/api/v1")
 public interface PrimeNumberOperation {
 
+    @GetMapping(path = "/primes", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     ResponseEntity<?> getPrimeNumbers();
 }
