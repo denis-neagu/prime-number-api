7f7685920e968dde328901d15e42cdc618670626
denis-neagu <denis.neagu@outlook.com>
2025-10-23 21:27:37 +0100

    add getPrimeNumbers declaration

--- src/main/java/com/denisneagu/primenumberapi/controller/operation/PrimeNumberOperation.java
+++ src/main/java/com/denisneagu/primenumberapi/controller/operation/PrimeNumberOperation.java
@@ -1,9 +1,12 @@
 package com.denisneagu.primenumberapi.controller.operation;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Validated
 @RequestMapping("/api/v1")
 public interface PrimeNumberOperation {
+
+    ResponseEntity<?> getPrimeNumbers();
 }
