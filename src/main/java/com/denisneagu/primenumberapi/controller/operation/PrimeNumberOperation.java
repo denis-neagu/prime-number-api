9d63bc8eaf6aa2f234dae7b4561960b080e8d40e
denis-neagu <denis.neagu@outlook.com>
2025-10-23 21:34:23 +0100

    add @ApiResponses

--- src/main/java/com/denisneagu/primenumberapi/controller/operation/PrimeNumberOperation.java
+++ src/main/java/com/denisneagu/primenumberapi/controller/operation/PrimeNumberOperation.java
@@ -1,6 +1,10 @@
 package com.denisneagu.primenumberapi.controller.operation;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -17,6 +21,19 @@ public interface PrimeNumberOperation {
                     Calculates and returns all prime numbers up to the given limit.
                     Supports optional selection of the algorithm selected and includes the time taken for computation.
                     """
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successful response",
+                            content = {
+                                    @Content(mediaType = "application/json", schema = @Schema()),
+                                    @Content(mediaType = "application/xml", schema = @Schema())
+                            }),
+                    @ApiResponse(responseCode = "400", description = "Bad request"),
+                    @ApiResponse(responseCode = "500", description = "Server error")
+            }
     )
     @GetMapping(path = "/primes", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     ResponseEntity<?> getPrimeNumbers();
