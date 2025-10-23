96659a2b9c096bf87ffbcea4caf022015b90fd8e
denis-neagu <denis.neagu@outlook.com>
2025-10-23 21:26:13 +0100

    add PrimeNumberOperation

---
+++ src/main/java/com/denisneagu/primenumberapi/controller/operation/PrimeNumberOperation.java
@@ -0,0 +1,9 @@
+package com.denisneagu.primenumberapi.controller.operation;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Validated
+@RequestMapping("/api/v1")
+public interface PrimeNumberOperation {
+}
