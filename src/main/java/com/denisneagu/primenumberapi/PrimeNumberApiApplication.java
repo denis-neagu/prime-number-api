2542bb2b4f1ab2d76eb94e43ff98941bf3ae8b1d
denis-neagu <denis.neagu@outlook.com>
2025-10-23 21:19:34 +0100

    init

---
+++ src/main/java/com/denisneagu/primenumberapi/PrimeNumberApiApplication.java
@@ -0,0 +1,12 @@
+package com.denisneagu.primenumberapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeNumberApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeNumberApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/denisneagu/primenumberapi/PrimeNumberApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denisneagu.primenumberapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeNumberApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
