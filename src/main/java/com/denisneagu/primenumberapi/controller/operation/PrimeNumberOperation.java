83e5dc8f612d205cb8d36265d8d6d4118f9a1f96
denis-neagu <denis.neagu@outlook.com>
2025-10-23 21:31:55 +0100

    add @Operation

--- src/main/java/com/denisneagu/primenumberapi/controller/operation/PrimeNumberOperation.java
+++ src/main/java/com/denisneagu/primenumberapi/controller/operation/PrimeNumberOperation.java
@@ -1,5 +1,6 @@
 package com.denisneagu.primenumberapi.controller.operation;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -10,6 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/api/v1")
 public interface PrimeNumberOperation {
 
+    @Operation(
+            summary = "Get all prime numbers up to and including the limit",
+            description = """
+                    Calculates and returns all prime numbers up to the given limit.
+                    Supports optional selection of the algorithm selected and includes the time taken for computation.
+                    """
+    )
     @GetMapping(path = "/primes", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     ResponseEntity<?> getPrimeNumbers();
 }
