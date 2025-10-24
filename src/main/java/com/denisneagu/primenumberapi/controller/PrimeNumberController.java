3fa2c40975e72e9a8c76ee22e5cbb86900cc4959
denis-neagu <denis.neagu@outlook.com>
2025-10-24 15:40:09 +0100

    add method call in controller

--- src/main/java/com/denisneagu/primenumberapi/controller/PrimeNumberController.java
+++ src/main/java/com/denisneagu/primenumberapi/controller/PrimeNumberController.java
@@ -3,6 +3,7 @@ package com.denisneagu.primenumberapi.controller;
 import com.denisneagu.primenumberapi.controller.operation.PrimeNumberOperation;
 import com.denisneagu.primenumberapi.dto.PrimeNumberResponse;
 import com.denisneagu.primenumberapi.enums.Algorithm;
+import com.denisneagu.primenumberapi.service.PrimeNumberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 public class PrimeNumberController implements PrimeNumberOperation {
+    private final PrimeNumberService primeNumberService;
 
     @Override
     public ResponseEntity<PrimeNumberResponse> getPrimeNumbers(long limit,
                                                                Algorithm algorithm,
                                                                boolean cache) {
-        log.info("Prime numbers finished calculating");
-        return null;
+        PrimeNumberResponse primeNumberResponse = primeNumberService.getPrimeNumbers(limit, algorithm, cache);
+        log.info("Finished calculating primes with limit: {}", limit);
+        return ResponseEntity.ok(primeNumberResponse);
     }
 }
