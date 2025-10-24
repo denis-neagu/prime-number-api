fc401ace50a0aad929efbdb3505fb7516d15ad4e
denis-neagu <denis.neagu@outlook.com>
2025-10-24 19:20:48 +0100

    add util methods to check memory

---
+++ src/main/java/com/denisneagu/primenumberapi/util/Util.java
@@ -0,0 +1,32 @@
+package com.denisneagu.primenumberapi.util;
+
+import com.denisneagu.primenumberapi.exception.MemoryConstraintException;
+
+public class Util {
+    private static final int MAX_SAFE_MEMORY_PERCENTAGE = 40;
+
+    public static String formatSizeInMbAndMiB(long sizeInBytes) {
+        double sizeInMiB = sizeInBytes / (1024.0 * 1024.0);
+        double sizeInMB = sizeInBytes / 1_000_000.0;
+        return String.format("%.4f MB and %.4f MiB (true value)", sizeInMB, sizeInMiB);
+    }
+
+    public static long getMaxMemory() {
+        return Runtime.getRuntime().maxMemory();
+    }
+
+    public static long getMaxSafeMemory() {
+        return (getMaxMemory() * MAX_SAFE_MEMORY_PERCENTAGE) / 100;
+    }
+
+    public static void checkMemorySafety(long bytesNeeded) {
+        long maxSafeMemory = getMaxSafeMemory();
+        if (bytesNeeded > maxSafeMemory) {
+            throw new MemoryConstraintException(
+                    String.format(
+                            "Requested memory for %s exceeds safe limit of %s",
+                            formatSizeInMbAndMiB(bytesNeeded),
+                            formatSizeInMbAndMiB(maxSafeMemory)));
+        }
+    }
+}
